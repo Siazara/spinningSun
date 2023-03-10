383e35ef5832938c5958a6c69acc7057825b2599
Rm4n <arman.nasirkhani@gmail.com>
2023-03-10 17:40:04 +0330

    ball aded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { useEffect } from "react"
-import styled, {keyframes} from "styled-components"
+import styled, { keyframes } from "styled-components"
 
+
+const sunWidth = window.screen.width
+const sunHeight = window.screen.height * (1 / 3)
+const ballHeight = window.screen.height * (1 / 2)
+const n = 6
+const thornL = 70
+const thornR = 10
+const sunR = 30
+const X = sunWidth / 2
+const Y = sunHeight / 2
+const clicked = false
+const ballStartX = X
+const ballStartY = ballHeight / 2
 
 const spin = keyframes`
 from {transform: rotate(0deg)}
@@ -14,47 +27,63 @@ animation-iteration-count: infinite;
 animation-timing-function: linear;
 `
 
-const width = window.screen.width
-const height = window.screen.height
-const n = 6
-const thornL = 70
-const thornR = 10
-const sunR = 30
+const drop = keyframes`
+from {transform: translate(${ballStartX}px, ${ballStartY}px)}
+to {transform: translate(${ballStartX}px, 0px)}
+`
+
+const Ball = styled.canvas`
+animation-name: ${clicked ? drop:null };
+animation-duration: ${10}s;
+animation-iteration-count: infinite;
+animation-timing-function: linear;
+`
 
 function App() {
 
-  const draw = async () => {
-  var ctx = document.getElementById('spinning-sun').getContext('2d')
+  const drawSun = async () => {
+    var ctx = document.getElementById('spinning-sun').getContext('2d')
 
-  const X = width / 2
-  const Y = height / 2
-
-  ctx.beginPath()
-  ctx.arc(X, Y, sunR, 0, 2 * Math.PI)
-  ctx.stroke()
-
-  for (let i = 0; i < n; i++) {
     ctx.beginPath()
-    let cos = Math.cos(i * 2 * Math.PI / n )
-    let sin = Math.sin(i * 2 * Math.PI / n )
-    ctx.moveTo(X + sunR * cos, Y + sunR * sin)
-    ctx.lineTo(X + (sunR + thornL) * cos, Y + (sunR + thornL) * sin)
+    ctx.arc(X, Y, sunR, 0, 2 * Math.PI)
     ctx.stroke()
 
+    for (let i = 0; i < n; i++) {
+      ctx.beginPath()
+      let cos = Math.cos(i * 2 * Math.PI / n)
+      let sin = Math.sin(i * 2 * Math.PI / n)
+      ctx.moveTo(X + sunR * cos, Y + sunR * sin)
+      ctx.lineTo(X + (sunR + thornL) * cos, Y + (sunR + thornL) * sin)
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.moveTo(X + (sunR + thornL) * cos, Y + (sunR + thornL) * sin)
+      ctx.arc(X + (sunR + thornL) * cos, Y + (sunR + thornL) * sin, thornR, 0, 2 * Math.PI)
+      ctx.fill()
+      ctx.stroke()
+    }
+  }
+
+  const drawBall = async () => {
+    var ctx = document.getElementById('ball').getContext('2d')
+
     ctx.beginPath()
-    ctx.moveTo(X + (sunR + thornL) * cos, Y + (sunR + thornL) * sin)
-    ctx.arc(X + (sunR + thornL) * cos, Y + (sunR + thornL) * sin, thornR, 0, 2 * Math.PI)
+    ctx.arc(ballStartX, ballStartY, thornR, 0, 2 * Math.PI)
     ctx.fill()
     ctx.stroke()
   }
-}
 
   useEffect(() => {
-    draw()
+    drawSun();
+    drawBall();
   })
 
   return (
-    <Spinningsun id='spinning-sun' width={width} height={height}></Spinningsun>
+    <div>
+      <Spinningsun id='spinning-sun' width={sunWidth} height={sunHeight}></Spinningsun>
+      {/* <Ball id='ball'></Ball> */}
+      <Ball id='ball' width={sunWidth} height={ballHeight}></Ball>
+    </div>
   );
 }
 
